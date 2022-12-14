d252ee3d9d6b78369e661660f1114067218a16c9
Erika Gutierrez <erikagutierrez@Erikas-MacBook-Pro.local>
2022-12-14 17:35:07 +0100

    setting up initial library structure

---
+++ PopularityContest/Get/data.py
@@ -0,0 +1,6 @@
+import spotipy
+import config
+from spotipy.oauth2 import SpotifyClientCredentials
+
+birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=config.id, client_secret=config.secret))
