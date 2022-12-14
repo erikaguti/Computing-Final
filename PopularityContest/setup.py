032fd44568ee3d3fea2a5b272985fd0cb9af7fac
Erika Gutierrez <erikagutierrez@Erikas-MacBook-Pro.local>
2022-12-14 17:59:36 +0100

    initial setup

---
+++ PopularityContest/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name= 'PopularityContest',
+      version='0.1',
+      packages=['PopularityContest', 
+      'Get', 
+      'Process', 
+      'Model'],
+    author='Erika and Tobi'
+)
