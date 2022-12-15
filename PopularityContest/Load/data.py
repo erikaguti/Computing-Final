60bfc9869bfd8189667a11c1fcd4916241eef12c
Erika Gutierrez <erikagutierrez@Erikas-MacBook-Pro.local>
2022-12-15 17:18:40 +0100

    get not top 50 and draft dataset

--- PopularityContest/Load/data.py
+++ PopularityContest/Load/data.py
@@ -3,7 +3,6 @@ import config
 import pandas as pd
 from spotipy.oauth2 import SpotifyClientCredentials
 
-charts = pd.read_csv('charts.csv')
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=config.id, client_secret=config.secret))
 
@@ -24,9 +23,9 @@ for track in playlisttracks['items']:
     trackids.append(track['track']['id'])
 
 
-audiofeatures = spotify.audio_features(trackids)
+Top50features = spotify.audio_features(trackids)
 
-songfeatures = pd.DataFrame(audiofeatures)
+songfeatures = pd.DataFrame(Top50features)
 
 
 '''
@@ -40,6 +39,69 @@ audioanalysisdf = pd.DataFrame()
 for item in audioanalysis:
     audioanalysis[item]
 '''
+
+
+categories = spotify.categories(country='ES', locale='en')
+
+categoryids = []
+for category in categories['categories']['items']:
+    if category['id'] != 'toplists':
+        print(category['name'], category['id'])
+        categoryids.append(category['id'])
+
+count = 0
+playlistids = []
+for id in categoryids:
+    playlist = spotify.category_playlists(category_id = id, country='ES', limit=5)
+    playlists = playlist['playlists']['items']
+    for item in playlists:
+        count = count + 1
+        playlistids.append(item['id'])
+
+tracks = []
+for id in playlistids:
+    playlisttracks = spotify.playlist_items(playlistid, fields='total, items(track.id)', limit = 5)
+    tracks.extend(playlisttracks['items'])
+
+ids = []
+for track in tracks:
+    ids.append(track['track']['id'])
+
+
+NotTop50features = []
+songs = len(ids)
+print(songs)
+offset = 0
+limit = 100
+while songs > 100:
+    NotTop50features.extend(spotify.audio_features(idstringer(ids[offset:limit])))
+    offset = offset + 100
+    songs = songs - 100
+    limit = limit + 100
+else:
+    NotTop50features.extend(spotify.audio_features(idstringer(ids[offset:limit+songs])))
+
+def idstringer(ids):
+    idstr = ''
+    for id in ids:
+        if id == ids[-1]:
+            idstr = idstr + id
+        else:
+            idstr = idstr + id + ','
+    return idstr
+
+
+songfeatures['Top50'] = 1
+notTop50 = pd.DataFrame(list(filter(lambda i:not(i == None), NotTop50features)))
+
+notTop50['Top50'] = 0
+
+pd.concat([songfeatures, notTop50]).to_csv('songfeatures.csv')
+
+
+
+
+
 
 
 
