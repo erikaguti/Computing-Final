d95859b6377a7b696e24ae797d1db15f7b774dd8
tobinho <tobias.pfeiffer@bse.eu>
2022-12-17 18:12:15 +0100

    graphical features

--- PopularityContest/Model/best_model.py
+++ PopularityContest/Model/best_model.py
@@ -43,3 +43,27 @@ def fit_n_predict(X_train, X_test, y_train, y_test, model):
     
     return fitted_model, predictions
 
+
+def conf_mat(y_test, model_predict):
+    cm = confusion_matrix(y_test, model_predict, labels=[0,1])
+    plt.figure(figsize=(10,7))
+    sns.heatmap(cm, cmap="seagreen", annot = True)
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+
+
+def plot_feats(fitted_model, X):
+    fitted_model.feature_importances_
+
+    features = X.columns
+
+    importances_clf = pd.Series(data = fitted_model.feature_importances_,
+                                index= features)
+
+    # Sort importances
+    importances_sorted_clf = importances_clf.sort_values()
+
+    # Draw a horizontal barplot of importances_sorted
+    importances_sorted_clf.plot(kind='barh', color='green')
+    plt.title('Gini Features Importances', size=15)
+    plt.show()
