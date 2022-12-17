489183f0288e0642d6070880c70515b2afc9daa9
Erika Gutierrez <erikagutierrez@Erikas-MacBook-Pro.local>
2022-12-17 12:01:24 +0100

    added skeleton of test functions

---
+++ test/Load/test_spotify.py
@@ -0,0 +1,19 @@
+import unittest
+import pandas as pd
+from  PopularityContest.Load.spotify import Spotify
+
+
+def test_getCategories():
+    return
+
+def test_getPlaylists():
+    return
+
+def test_getSongs():
+    return
+
+def test_getSongFeatures():
+    return 
+
+def test_idstringer():
+    return
